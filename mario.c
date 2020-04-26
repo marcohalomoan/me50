afa0360854346a7117303aad24e96ff703276dca
marcohalomoan <marcohalomoan@users.noreply.github.com>
2020-04-26 19:07:57 +0000

    automated commit by check50 [check50=True] [lang=C.UTF-8]

--- mario.c
+++ mario.c
@@ -8,7 +8,7 @@ int main (void)
     {
         for (int i = 0; i < n; i++)
         {
-            for (int k = 0; k <= n-i-1 ; k++)
+            for (int k = 0; k < n-i-1 ; k++)
             {
                 printf(" ");
             }
