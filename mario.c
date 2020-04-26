4787ab11f962857bdf45f5bbae529488e13665f2
marcohalomoan <marcohalomoan@users.noreply.github.com>
2020-04-26 19:14:57 +0000

    automated commit by check50 [check50=True] [lang=C.UTF-8]

--- mario.c
+++ mario.c
@@ -4,23 +4,20 @@
 int main (void)
 {
     int n = get_int("Height = ");
-    if (n >= 1 && n <= 8)
-    {
-        for (int i = 0; i < n; i++)
-        {
-            for (int k = 0; k < n-i-1 ; k++)
-            {
-                printf(" ");
-            }
-            for (int j = 0; j <= i; j++)
-            {
-                printf("#");
-            }
-            printf("\n");
-        }
-    }
-    else
+    while (n < 1 || n > 8)
     {
         n = get_int("Height = ");
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int k = 0; k < n-i-1 ; k++)
+        {
+            printf(" ");
+        }
+        for (int j = 0; j <= i; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
     }
 }
