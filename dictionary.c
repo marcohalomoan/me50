66587ecafc1c5a544dc4d0e79b5b5b33fceb3d10
marcohalomoan <marcohalomoan@users.noreply.github.com>
2020-06-05 06:18:39 +0000

    automated commit by check50 [check50=True]

--- dictionary.c
+++ dictionary.c
@@ -21,7 +21,7 @@ node;
 int count_size = 0;
 
 // Number of buckets in hash table
-const unsigned int N = 26;
+const unsigned int N = 65536;
 
 // Hash table
 node *table[N];
@@ -30,8 +30,7 @@ node *table[N];
 bool check(const char *word)
 {
     int index = hash(word);
-    node *checker = malloc(sizeof(node));
-    checker = table[index];
+    node *checker = table[index];
     while(checker->next != NULL)
     {
         if(strcasecmp(word, checker->word) == 0)
@@ -45,21 +44,14 @@ bool check(const char *word)
 
 // Hashes word to a number
 // https://cs50.stackexchange.com/questions/37209/pset5-speller-hash-function
-unsigned int hash(const char *word)
+unsigned int hash(const char* word)
 {
-    int hash = 0;
-    if(isalpha(word[0]))
+    unsigned int hash_value = 0;
+    for (int i = 0, n = strlen(word); i < n; i++)
     {
-        if (isupper(word[0]))
-        {
-            hash = word[0] - 65;
-        }
-        else if (islower(word[0]))
-        {
-            hash = word[0] - 97;
-        }
+        hash_value = (hash_value << 2) ^ word[i];
     }
-    return hash;
+    return hash_value % N; //N is size of hashtable
 }
 
 // Loads dictionary into memory, returning true if successful else false
@@ -84,8 +76,10 @@ bool load(const char *dictionary)
         n->next = table[index];
         table[index] = n;
         count_size++;
+        free(n);
     }
     fclose(f);
+    free(word);
     return true;
 }
 
@@ -98,12 +92,10 @@ unsigned int size(void)
 // Unloads dictionary from memory, returning true if successful else false
 bool unload(void)
 {
-    node *checker = malloc(sizeof(node));
-    node *deleter = malloc(sizeof(node));
     for(int i = 0; i < N; i++)
     {
-        checker = table[i];
-        deleter = table[i];
+        node *checker = table[i];
+        node *deleter = table[i];
         while(checker->next != NULL)
         {
             checker = checker->next;
